a2b84f55a7f5c1a300b9e5b5e92401f1ba98481f
Chadwick <83472455+productivechad@users.noreply.github.com>
2021-10-01 10:10:28 -0400

    Add files via upload

---
+++ HelloWorld!/HelloWorld!/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main()
+        {
+        Console.WriteLine("Acme Accounting Systems");
+        Console.WriteLine("Remember, we’re “accounting” on you! ");
+        Console.ReadLine();
+    }
+    }
+
