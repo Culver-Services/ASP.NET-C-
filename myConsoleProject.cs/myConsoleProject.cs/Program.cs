72d7935c998f4f3e534ad419aaca9bc03872cabf
Chadwick <83472455+productivechad@users.noreply.github.com>
2021-10-01 10:10:01 -0400

    Add files via upload

---
+++ myConsoleProject.cs/myConsoleProject.cs/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace myConsoleProject.cs_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello, " + name + "!");
+            Console.Read();
+
+
+
+        }
+    }
+}
