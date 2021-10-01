f5c4cbc0ce4771fe8b075bf2a8a0e8b81ccb23e9
Chadwick <83472455+productivechad@users.noreply.github.com>
2021-10-01 15:23:29 -0400

    Add files via upload

---
+++ Math and Comparison Operations/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Math_and_Comparison_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int total = 5 + 10;
+            //Console.WriteLine("Five plus Ten = " + total.ToString());
+
+            ////or
+
+            //int total = 5 + 10;
+            //int otherTotal = 12 + 22;
+            //int combined = total + otherTotal;
+            //Console.WriteLine(combined);
+            //Console.WriteLine();
+
+            //int difference = 10 - 5;
+            //Console.WriteLine("Five minus Ten = " + difference.ToString());
+            //Console.ReadLine();
+
+            //int product = 10 * 5;
+            //Console.WriteLine(product);
+            //Console.ReadLine();
+
+            //int quotient = 100 / 5;
+            //Console.WriteLine(quotient);
+            //Console.ReadLine();
+
+            //double quotient = 100.0 / 17.0;
+            //Console.WriteLine(quotient);
+            //Console.ReadLine();
+
+            //int remainder = 11 % 2;
+            //Console.WriteLine(remainder);
+            //Console.ReadLine();
+
+            //bool trueOrfalse = 12 < 5;
+            //Console.WriteLine(trueOrfalse.ToString());
+            //Console.ReadLine();
+
+            int roomTempurature = 70;
+            int currentTempurature = 70;
+
+            //bool isWarm = currentTempurature >= roomTempurature;
+            bool isWarm = currentTempurature == roomTempurature;
+            Console.WriteLine(isWarm);
+            Console.ReadLine();
+
+            // != is used to determine if two numbers are not equivalent
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
