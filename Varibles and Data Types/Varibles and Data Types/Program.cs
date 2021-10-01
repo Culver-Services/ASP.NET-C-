34d108f2cc9810dbbd1fa5345068017675d53dd2
Chadwick <83472455+productivechad@users.noreply.github.com>
2021-10-01 10:40:35 -0400

    Add files via upload

---
+++ Varibles and Data Types/Varibles and Data Types/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Varibles_and_Data_Types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //This is a simple program
+            //Console.WriteLine("What is your name?");
+            //string yourName = Console.ReadLine();
+            //Console.WriteLine("Your name is: " + yourName);
+            //Console.ReadLine();
+
+            //Console.WriteLine("What is your favourite number?");
+            //string favouriteNumber = Console.ReadLine();
+            //Console.WriteLine("Your favourite number plus 5 is: " + total);
+            //Console.ReadLine();
+
+            //bool isStudying = false;
+            //byte hoursWorked = 42;
+            //sbyte currentTemperature = -23;
+            //char questionMark = '\u2103';
+
+            //Console.WriteLine(questionMark);
+            //Console.ReadLine();
+
+            decimal moneyInBank = 100.5m;
+
+            double heightInCentimeters = 211.30202092;
+
+            float secondLeft = 2.62f;
+
+            short temperatureOnMars = -341;
+
+            string myName = "Chad";
+
+            int currntAge = 28;
+            string yearOld = currentAge.ToString(isRaining);
+            Console.WriteLine(rainingStatus);
+            Console.ReadLine();
+
+        }
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+          
+
+    }
+}
