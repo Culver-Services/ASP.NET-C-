d77f6cef898c9e5d3383eb08df51b5e9e39cc57a
Chadwick <83472455+productivechad@users.noreply.github.com>
2021-10-07 12:24:42 -0400

    Add files via upload

---
+++ Boolean Operators/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Boolean_Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+
+            Console.WriteLine(true && true);    // output: True
+            Console.WriteLine(true && false);   // output: False
+            Console.WriteLine(false && true);   // output: False
+            Console.WriteLine(false && false);  // output: False
+
+            Console.WriteLine(true || true);    // output: True
+            Console.WriteLine(true || false);   // output: True
+            Console.WriteLine(false || true);   // output: True
+            Console.WriteLine(false || false); // output: False
+
+            Console.WriteLine(true != true);    // output: False
+            Console.WriteLine(true != false);   // output: True
+            Console.WriteLine(false != true);   // output: True
+            Console.WriteLine(false != false);  // output: False
+
+            Console.WriteLine(true ^ true);    // output: False
+            Console.WriteLine(true ^ false);   // output: True
+            Console.WriteLine(false ^ true);   // output: True
+            Console.WriteLine(false ^ false);  // output: False
+        }
+    }
+}
