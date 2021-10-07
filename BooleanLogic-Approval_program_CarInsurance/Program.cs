b83a4b95bd2b9d6498dc4a1e246562ae7e03a8e6
Chadwick <83472455+productivechad@users.noreply.github.com>
2021-10-07 12:30:28 -0400

    Add files via upload

---
+++ BooleanLogic-Approval_program_CarInsurance/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BooleanLogic_Approval_program_CarInsurance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Have you ever had a DUI?");
+            Console.WriteLine("true or false:");
+            bool dui = bool.Parse(Console.ReadLine());
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            int ticket = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Qualified?");
+            Console.WriteLine(age > 15 && ticket < 3 && dui == false);
+            Console.ReadLine();
+        }
+    }
+}
