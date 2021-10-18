75fdfd2ff6cd9413b722e3065c343cf0bed32f1f
Chadwick Culver <83472455+productivechad@users.noreply.github.com>
2021-10-18 00:10:40 -0400

    Add files via upload

---
+++ Operator Overloading/Employee.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operator_Overloading
+{
+    class Employee : Person
+    {
+
+        public int Id = 0;
+
+        //Calls the base constructor (Person)
+        public Employee(string FirstName, string LastName) : base(FirstName, LastName)
+        {
+
+        }
+
+        public static bool operator ==(Employee a, Employee b)
+        {
+            return a.Id == b.Id;
+        }
+
+        public static bool operator !=(Employee a, Employee b)
+        {
+            return a.Id != b.Id;
+        }
+
+        public void Quit()
+        {
+            Console.WriteLine("Press Enter to Exit.");
+            Console.Read();
+            Environment.Exit(0);
+        }
+
+        //Override base method 
+        public override void SayName()
+        {
+            Console.WriteLine("Name: " + "[" + base.FirstName + " " + base.LastName + "]");
+        }
+    }
+}
---
+++ Operator Overloading/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Operator_Overloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp1 = new Employee("Same", "Person");
+            Employee emp2 = new Employee("Same", "Person");
+            Employee emp3 = new Employee("Different", "Person");
+            emp3.Id = 1;
+
+            Console.WriteLine("Comparing first two employees: ");
+            Console.WriteLine("Result: " + Convert.ToString(emp1 == emp2));
+            Console.WriteLine("Comparing second and third employees: ");
+            Console.WriteLine("Result: " + Convert.ToString(emp3 == emp2));
+            Console.ReadLine();
+        }
+    }
+}
