0be7cb8888916c69e0b6ded395ea03e04604890f
Chadwick Culver <83472455+productivechad@users.noreply.github.com>
2021-10-18 18:21:49 -0400

    Add files via upload

---
+++ StructConsole/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number num1 = new Number();
+            num1.Amount = 81.81m;
+
+            Console.WriteLine(num1.Amount);
+            Console.ReadLine();
+        }
+    }
+}
