6554da5ce13a4b4f0085a4a7075a45b504481f34
Chadwick Culver <83472455+productivechad@users.noreply.github.com>
2021-10-19 21:28:04 -0400

    Add files via upload

---
+++ File-IO-ConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace File_IO_ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            Console.WriteLine("Please enter a number:");
+            string number = string.Format(Console.ReadLine());
+            File.WriteAllText(@"C:\Users\Chadp\Documents\logs\number.txt", number);
+            string text = File.ReadAllText(@"C:\Users\Chadp\Documents\logs\number.txt");
+            Console.WriteLine(text);
+            Console.ReadLine();
+        }
+    }
+}
